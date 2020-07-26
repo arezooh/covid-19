2faa4530c1ed6d5b5ca180fe30056a6bf95b6d0b
arezooh <48172387+arezooh@users.noreply.github.com>
2020-07-26 20:29:32 +0430

    update real and prediction plot
    Former-commit-id: 24766cda760b2cf0899af757e1b7cfbdd83b5f05

--- server ML code/prediction.py
+++ server ML code/prediction.py
@@ -475,41 +475,21 @@ def real_prediction_plot(df,r,target_name,best_h,spatial_mode,methods,numberOfSe
         os.makedirs(address)
 
     for method in methods:
-
-        data=makeHistoricalData(best_h[method]['MAPE'], r, target_name, 'mrmr', spatial_mode, target_mode, './')
-        if numberOfSelectedCounties == -1 :
-          numberOfSelectedCounties = len(data['county_fips'].unique())
-        data = data.sort_values(by=['county_fips', 'date of day t'])
-        data = data[(data['county_fips'] <= data['county_fips'].unique()[numberOfSelectedCounties - 1])]
-        data = data.reset_index(drop=True)
-        data = data[['state_fips','county_name','county_fips','date of day t','Target']]
-        data=data.sort_values(by=['date of day t','county_fips'])
-        data_train_train=data.iloc[:-2*(r*numberOfSelectedCounties),:]
-        data_train_val=data.iloc[-2*(r*numberOfSelectedCounties):-(r*numberOfSelectedCounties),:]
-        data_test=data.tail(r*numberOfSelectedCounties)
-        if spatial_mode == 'country' :
-            data_train_train=data_train_train.sort_values(by=['county_fips','date of day t'])
-            data_train_val=data_train_val.sort_values(by=['county_fips','date of day t'])
-            data_test=data_test.sort_values(by=['county_fips','date of day t'])
-            data=data_train_train.append(data_train_val)
-            data=data.append(data_test)
-        if spatial_mode == 'county' :
-            data_train = data_train_train.append(data_train_val)
-            data_train = data_train.sort_values(by=['county_fips','date of day t'])
-            data_test = data_test.sort_values(by=['county_fips','date of day t'])
-            data = data_train.append(data_test)
-        if spatial_mode == 'state' :
-            data_train = data_train_train.append(data_train_val)
-            data_train = data_train.sort_values(by=['state_fips','date of day t'])
-            data_test = data_test.sort_values(by=['state_fips','date of day t'])
-            data = data_train.append(data_test)
-        method_prediction_df = pd.DataFrame(df[method],columns=[method])
-        df_for_plot = pd.concat([data.reset_index(drop=True),method_prediction_df.reset_index(drop=True)],axis=1)
+        
+        method_prediction_df = df[method] # this df contain real and predicted target values
+        county_name_df=pd.read_csv('./'+'fixed-data.csv')[['county_fips','county_name']] # we need county names for plot label
+        df_for_plot = pd.merge(method_prediction_df,county_name_df,how='left')
 
         df_for_plot['date'] = df_for_plot['date of day t'].apply(lambda x:datetime.datetime.strptime(x,'%m/%d/%y')+datetime.timedelta(days=r))
         df_for_plot['date'] = df_for_plot['date'].apply(lambda x:datetime.datetime.strftime(x,'%m/%d/%y'))
 
-        counties = [36061]+random.sample(df_for_plot['county_fips'].unique().tolist(),2) # newyork + two random county
+        counties = []
+        for i in [36061,40117,51059]: # newyork + two random county
+          if len(df_for_plot[df_for_plot['county_fips']==i]) > 0 :
+              counties.append(i)
+          else :
+              counties = counties + random.sample(df_for_plot['county_fips'].unique().tolist(),1)
+
 
         length=list()
         for county in counties:
@@ -528,10 +508,10 @@ def real_prediction_plot(df,r,target_name,best_h,spatial_mode,methods,numberOfSe
             plt.plot(df_for_plot.loc[df_for_plot['county_fips']==county,'date'],df_for_plot.loc[df_for_plot['county_fips']==county,'Target'],label='Real values',linewidth=2.0)
             plt.xticks(rotation=65)
             fig.subplots_adjust(hspace=0.4)
-            plt.ylabel('Number of deaths')
+            plt.ylabel('Number of confirmed')
             countyname = df_for_plot.loc[df_for_plot['county_fips']==county,'county_name'].unique()
-            if len(countyname)>0 : # it is False when newyork is not in selected counties and make error
-              plt.title(df_for_plot.loc[df_for_plot['county_fips']==county,'county_name'].unique()[0])
+            # if len(countyname)>0 : # it is False when newyork is not in selected counties and make error
+            plt.title(df_for_plot.loc[df_for_plot['county_fips']==county,'county_name'].unique()[0])
             plt.legend()
         plt.xlabel('Date')
         plt.savefig(address + str(method) + ' real_prediction_values.jpg')
