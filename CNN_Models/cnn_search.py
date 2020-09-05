076e1b22092618ed0f1b028cb4facdef926caf55
HadiFazelinia <hadifazelinia78@gmail.com>
2020-09-05 19:25:22 +0430

    change county_predict variable from map into hash_array in cnn_search
    Former-commit-id: 2c8fb06e9d215ae3b8ea2cfdf6c58eaae390c8db

--- CNN_Models/cnn_search.py
+++ CNN_Models/cnn_search.py
@@ -437,10 +437,7 @@ def evaluate_data(model, x_test, y_test, input_size, normal_min, normal_max):
 
     # init counties_predict array
     counties_predict = []
-    counties_predict_per_day = {}
-    counties = loadCounties(_CONUTIES_FIPS_)
-    for county in counties:
-        counties_predict_per_day[county['county_fips']] = 0
+    counties_predict_per_day = zeros(78031)
 
     for _ in range(21):
         counties_predict.append(counties_predict_per_day.copy())
@@ -468,7 +465,7 @@ def evaluate_data(model, x_test, y_test, input_size, normal_min, normal_max):
                 sum_MASE += abs(y_test_org[k][i][j][0] - x_test[k][i][j][-4])
 
                 for county in distribution[k][i][j]:
-                    counties_predict[k][str(county['fips'])] += round(subY_predict[k][0][0][0] * county['percent'])
+                    counties_predict[k][county['fips']] += round(subY_predict[k][0][0][0] * county['percent'])
 
     MAE_county, MAPE_county, MASE_county = calculate_county_error(distribution_no_days - 21 - 20, counties_predict)
 
