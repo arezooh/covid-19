52220dbf7eb0aa900d033baa5c90e8373b312a24
HadiFazelinia <hadifazelinia78@gmail.com>
2020-09-09 18:46:25 +0430

    add single_model.py

---
+++ CNN_Models/single_model.py
@@ -0,0 +1,222 @@
+################################################################ Imports
+
+import cnn_search
+
+from math import log2, floor, ceil, sqrt
+from numpy import array, zeros, save, load, copyto, append
+from os import getpid
+import os
+import datetime
+import matplotlib.pyplot as plt
+
+single_model_parameters = (3, 0, 0, 3, 0)
+image_size = 300
+
+# Use this function to log states of code, helps to find bugs
+def log(str):
+    t = datetime.datetime.now().isoformat()
+    with open('log_sd', 'a') as logFile:
+        logFile.write('[{0}][{1}] {2}\n'.format(t, getpid(), str))
+
+def pad_subImage(data, input_size, minX, minY, maxX, maxY):
+    data_shape = data.shape
+    padded_data = []
+
+    for i in range(data_shape[0]):
+        padded_data.append(cnn_search.pad_data(data[i], input_size))
+
+    n = input_size // 2
+    padded_data = array(padded_data)
+    return padded_data[0:data_shape[0], minX:maxX + 2 * n, minY:maxY + 2 * n, 0:data_shape[3]] 
+
+# This function extract windows with "input_size" size from image, evaluate model with the windows data
+# Note: the data in here is padded.
+def evaluate_data_sd(model, x_data, y_data, input_size, normal_min, normal_max):
+    data_shape = x_data.shape
+    y_data_org = cnn_search.inverse_normal_y(y_data, normal_min, normal_max)
+
+    sum_org = 0
+    sum_predict = 0
+    sum_simple = 0
+    sum_MAE = 0
+    sum_MAPE = 0
+    sum_MASE = 0
+
+    # init counties_predict array
+    counties_predict = []
+    counties_predict_per_day = zeros(78031)
+
+    for _ in range(data_shape[0]):
+        counties_predict.append(counties_predict_per_day.copy())
+
+    # load distribution
+    distribution = cnn_search.loadJsonFile(cnn_search._DISTRIBUTION_FILENAME_)
+    # no need of first 14 days distribution
+    distribution = distribution[14:]
+
+    for i in range(data_shape[1] - (input_size // 2 * 2)):
+        for j in range(data_shape[2] - (input_size // 2 * 2)):
+            subX = x_data[0:data_shape[0], i:i+input_size, j:j+input_size, 0:data_shape[3]]
+
+            subY_predict_normal = model.predict(subX)
+            pred_shape = subY_predict_normal.shape
+            subY_predict = cnn_search.inverse_normal_y(subY_predict_normal, normal_min, normal_max)
+
+            for k in range(pred_shape[0]):
+                sum_org += y_data_org[k][i][j][0]
+                sum_predict += subY_predict[k][0][0][0]
+                sum_simple += x_data[k][i][j][-4]
+                sum_MAE += abs(y_data_org[k][i][j][0] - subY_predict[k][0][0][0])
+                sum_MAPE += abs(y_data_org[k][i][j][0] - subY_predict[k][0][0][0])
+                sum_MASE += abs(y_data_org[k][i][j][0] - x_data[k][i][j][-4])
+
+                for county in distribution[k][i][j]:
+                    counties_predict[k][county['fips']] += round(subY_predict[k][0][0][0] * county['percent'])
+
+    # debug: error calculation must be corrected
+    MAE_county, MAPE_county, MASE_county = cnn_search.calculate_county_error(0, counties_predict)
+
+    MAE_pixel = sum_MAE / (data_shape[0] * data_shape[1] * data_shape[2])
+    MAPE_pixel = sum_MAPE / sum_org
+    MASE_pixel = MAE_pixel / (sum_MASE / (data_shape[0] * data_shape[1] * data_shape[2]))
+
+    MAE_country = abs(sum_org - sum_predict)
+    MAPE_country = abs(sum_org - sum_predict) / sum_org
+    MASE_country = MAE_country / abs(sum_simple - sum_predict)
+
+    results = (MAE_pixel, MAPE_pixel, MASE_pixel, MAE_country, MAPE_country, MASE_country, MAE_county, MAPE_county, MASE_county)
+
+    return (counties_predict, results)
+
+def county_pixcels(county_fips):
+    gridIntersection = cnn_search.loadJsonFile(cnn_search._GRID_INTERSECTION_FILENAME_)
+    pixcels = []
+
+    for x in range(len(gridIntersection)):
+        for y in range(len(gridIntersection[x])):
+            for county in gridIntersection[x][y]:
+                if county['fips'] == county_fips:
+                    pixcels.append((x, y, county_fips))
+
+    return(pixcels)
+
+def plot_chart(fips, prediction, original):
+    start_day = (cnn_search.startDay + datetime.timedelta(days=14))
+    days = []
+    for i in range(len(prediction)):
+        days.append((start_day + datetime.timedelta(days=i)).strftime('%m-%d'))
+
+    fig, ax = plt.subplots()
+
+    ax.tick_params(axis = 'x', labelrotation = 60, labelsize=8)
+    ax.plot(days, original, color='red', linestyle='-', label='original')
+    ax.plot(days[:-20], prediction[:-20], color='blue', linestyle='-', label='train')
+    ax.plot(days[-21:], prediction[-21:], color='green', linestyle='-', label='test')
+    ax.set(ylabel='death')
+
+    fig.set_figheight(5)
+    fig.set_figwidth(25)
+    plt.legend()
+
+    fig.savefig('result_{0}.png'.format(fips))
+
+def predict_counties_result(counties_fips, normal_x_data, normal_y_data, input_size, normal_min, normal_max):
+    pixcels = []
+    min_x = -1
+    min_y = -1
+    max_x = -1
+    max_y = -1
+
+    for fips in counties_fips:
+        pixcels.extend(county_pixcels(fips))
+
+    for pixcel in pixcels:
+        if (pixcel[0] == -1 or pixcel[0] < min_x):
+            min_x = pixcel[0]
+        if (pixcel[1] == -1 or pixcel[1] < min_y):
+            min_y = pixcel[1]
+        if (pixcel[0] == -1 or pixcel[0] > max_x):
+            max_x = pixcel[0]
+        if (pixcel[1] == -1 or pixcel[1] > max_y):
+            max_y = pixcel[1]
+
+    counties_predict, result = evaluate_data_sd(model, 
+        pad_subImage(normal_x_data, input_size, min_x, min_y, max_x, max_y),
+        pad_subImage(normal_y_data, input_size, min_x, min_y, max_x, max_y),
+        input_size,
+        normal_min,
+        normal_max)
+
+    for fips in counties_fips:
+        # debug: last parameter should be original data, which will get from internal calculate_county_error
+        plot_chart(fips, counties_predict[:, fips], counties_predict[:, fips])
+
+    return result
+
+################################################################ main
+
+if __name__ == "__main__":
+    a = array([1,2,3,4])
+    print(a)
+    exit(0)
+    cnn_search.init_hashCounties()
+
+    # Check if instances are ready
+    if (os.path.exists('x_' + cnn_search._INSTANCES_FILENAME_) and os.path.exists('y_' + cnn_search._INSTANCES_FILENAME_)):
+        log('instances found')
+    else:
+        raise Exception('no instances found')
+
+    x_instances = load('x_' + cnn_search._INSTANCES_FILENAME_)
+    y_instances = load('y_' + cnn_search._INSTANCES_FILENAME_)
+
+    ################################################################ split imageArray into train, validation and test
+
+    log('spliting data into train, validation and test')
+
+    x_dataTrain = x_instances[:-63]
+    y_dataTrain = y_instances[:-63]
+
+    x_dataValidation = x_instances[-63:-42]
+    y_dataValidation = y_instances[-63:-42]
+
+    x_dataTest = x_instances[-42:-21]
+    y_dataTest = y_instances[-42:-21]
+
+    x_dataFinalTest = x_instances[-21:]
+    y_dataFinalTest = y_instances[-21:]
+
+    ################################################################ normalize data
+
+    log('normalizing data')
+
+    normal_x_dataTrain, normal_x_dataValidation, normal_x_dataTest, normal_x_dataFinalTest = cnn_search.normal_x(x_dataTrain, x_dataValidation, x_dataTest, x_dataFinalTest)
+    normal_y_dataTrain, normal_y_dataValidation, normal_y_dataTest, normal_y_dataFinalTest, normal_min, normal_max = cnn_search.normal_y(y_dataTrain, y_dataValidation, y_dataTest, y_dataFinalTest)
+
+    ################################################################ clearing memory
+
+    del x_instances, x_dataTrain, x_dataValidation, x_dataTest, x_dataFinalTest
+    del y_instances, y_dataTrain, y_dataValidation, y_dataTest, y_dataFinalTest
+
+    log('Phase of testing the model started')
+
+    ################################################################ model execution
+
+    input_size = single_model_parameters[0]
+    hidden_dropout = single_model_parameters[1]
+    visible_dropout = single_model_parameters[2]
+    NO_dense_layer = single_model_parameters[3]
+    increase_filters = single_model_parameters[4]
+
+    log('Model testing with parameters {0}'.format(single_model_parameters))
+    NO_blocks = floor(log2(input_size))
+    model = cnn_search.create_model(input_size, hidden_dropout, visible_dropout, NO_blocks, NO_dense_layer, increase_filters)
+    cnn_search.train_data(model, normal_x_dataTrain, normal_y_dataTrain, normal_x_dataValidation, normal_y_dataValidation, 2, input_size)
+    result = cnn_search.evaluate_data(model, normal_x_dataTest, normal_y_dataTest, input_size, normal_min, normal_max)
+
+    log('result for Pixels, MAE:{0}, MAPE:{1}, MASE:{2}'.format(result[0], result[1], result[2]))
+    log('result for Country, MAE:{0}, MAPE:{1}, MASE:{2}'.format(result[3], result[4], result[5]))
+    log('result for County, MAE:{0}, MAPE:{1}, MASE:{2}'.format(result[6], result[7], result[8]))
+
+
+        
