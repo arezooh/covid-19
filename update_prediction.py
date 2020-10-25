10647344ce9c0ef22a5889826bb0c91e829cb7fb
ECE_Cluster_R1_003 <arezo.h1371@yahoo.com>
2020-10-24 19:30:19 -0700

    'Predictions updated'

--- update_prediction.py
+++ update_prediction.py
@@ -275,7 +275,7 @@ def main():
     weekly_output_csv.to_csv("US-Weekly-Deaths-Prediction.csv")
     scenarios_output_csv.to_csv("Different-Scenarios.csv", index=False)
     
-    push("Prediction!s updated")
+    push("Predictions updated")
 
 if __name__ == "__main__":
     main()
