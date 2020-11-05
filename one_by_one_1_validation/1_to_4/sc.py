bdddfa9122e90c6be866429ad1450ea12a4dbb43
ECE_Cluster_R1_003 <arezo.h1371@yahoo.com>
2020-11-05 15:02:44 -0800

    updated data
    Former-commit-id: cc29df2a891ef9698fb7b17982f0728f1672ef53

--- one_by_one_1_validation/1_to_4/sc.py
+++ one_by_one_1_validation/1_to_4/sc.py
@@ -4,15 +4,12 @@ import os
 import subprocess
 from pexecute.process import ProcessLoom
 
-def Run(i):
-    subprocess.call("python ./prediction.py "+str(i), shell=True)
 
 def main():
-    loom = ProcessLoom(max_runner_cap = 8)
+    
     for i in range(4):
         print(i)
-        loom.add_function(Run,[i])
-    loom.execute()
+        subprocess.call("python ./prediction.py "+str(i), shell=True)
 
 
 if __name__ == "__main__":
