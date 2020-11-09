5f7421387986065a54cf143011c0b26566451316
maryam97 <marmegh@gmail.com>
2020-11-10 00:58:42 +0330

    fix script
    Former-commit-id: 567bbd8ff4973a050b643895cf10e1632776f50b

--- each_country_one_by_one/sc.py
+++ each_country_one_by_one/sc.py
@@ -6,13 +6,13 @@ from pexecute.process import ProcessLoom
 
 
 def main():
-    
+    print('countrycountry:', argv[1:][0])
+    arg = "'" + argv[1:][0] + "'"
+    print('arg: ', arg)
     for i in range(7):
-        print('countrycountry:',argv[1:][0])
-        subprocess.call("python ./prediction.py "+str(i)+" "+argv[1:][0], shell=True)
-    subprocess.call("python ./errors.py "+argv[1:][0], shell=True)
+        subprocess.call("python ./prediction.py " + str(i) + " " + arg, shell=True)
+    subprocess.call("python ./errors.py " + arg, shell=True)
+
 
 if __name__ == "__main__":
-    
-    
     main()
